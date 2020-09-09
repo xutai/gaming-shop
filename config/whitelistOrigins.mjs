ffebd56e26e50ff2d0583a9c156b8d21363ba686
xutai <xutai@github.com>
2020-09-09 13:27:24 +0800

    fix

--- config/whitelistOrigins.mjs
+++ config/whitelistOrigins.mjs
@@ -5,4 +5,10 @@ export const whitelistOrigins = [
     'http://localhost:8081',
     'http://localhost:8082',
     'http://localhost:8083',
+    'http://xutai.site:3000',
+    'http://xutai.site:5000',
+    'http://xutai.site:8080',
+    'http://xutai.site:8081',
+    'http://xutai.site:8082',
+    'http://xutai.site:8083',
 ]
