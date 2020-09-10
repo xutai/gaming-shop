ae9feb2a60bb9dc6f10f82045ca81b32a4046632
xutai <xutai@github.com>
2020-09-10 21:21:44 +0800

    fix

--- mixins/set-cookie.mjs
+++ mixins/set-cookie.mjs
@@ -15,17 +15,17 @@ const setCookiePreset = (obj) => {
     }
     return (
         [
-            "session=session-routes-signup-namecheck-file-join.js; SameSite=Lax;",
-            `username=${obj.username}; Expires=${expiredDate}; path=/; SameSite=Lax; Domain=xutai.site; `,
-            `username=${obj.username}; Expires=${expiredDate}; path=/; SameSite=Lax; Domain=localhost; `,
-            `loginToken=${obj.password}; Expires=${expiredDate}; path=/; SameSite=Lax; HttpOnly; Domain=xutai.site;`,
-            `loginToken=${obj.password}; Expires=${expiredDate}; path=/; SameSite=Lax; HttpOnly; Domain=localhost;`,
-            `accessToken=${obj.password}_${Date.now()}; Expires=${expiredDate}; path=/; SameSite=Lax; HttpOnly; Domain=xutai.site;`,
-            `accessToken=${obj.password}_${Date.now()}; Expires=${expiredDate}; path=/; SameSite=Lax; HttpOnly; Domain=localhost;`,
+            "session=session-routes-signup-namecheck-file-join.js; SameSite=Lax",
+            `username=${obj.username}; Expires=${expiredDate}; Domain=xutai.site;  path=/; SameSite=Lax`,
+            `username=${obj.username}; Expires=${expiredDate}; Domain=localhost;  path=/; SameSite=Lax`,
+            `loginToken=${obj.password}; Expires=${expiredDate}; Domain="xutai.site"; path=/; SameSite=Lax; HttpOnly`,
+            `loginToken=${obj.password}; Expires=${expiredDate}; Domain=localhost; path=/; SameSite=Lax; HttpOnly`,
+            `accessToken=${obj.password}_${Date.now()}; Expires=${expiredDate}; Domain=xutai.site;path=/; SameSite=Lax; HttpOnly`,
+            `accessToken=${obj.password}_${Date.now()}; Expires=${expiredDate}; Domain=localhost;path=/; SameSite=Lax; HttpOnly`,
             // `password=${obj.password}; Expires=Tue, 30 July 2020 14:41:44 GMT; path=/; HttpOnly`,
             // `password=${obj.password}; Expires=Tue, 30 July 2020 14:41:44 GMT; Secure;`,
-            `__Host-cookiePrefix=cookiePrefix; Expires=${expiredDate}; secure; path=/; SameSite=Lax;`,
-            `__Secure-cookiePrefix=cookiePrefix; Expires=${expiredDate}; secure; SameSite=Lax;`,
+            `__Host-cookiePrefix=cookiePrefix; Expires=${expiredDate}; secure; path=/; SameSite=Lax`,
+            `__Secure-cookiePrefix=cookiePrefix; Expires=${expiredDate}; secure; SameSite=Lax`,
         ]
     )
 
