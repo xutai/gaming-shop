657a8b04e5fb13f5c24bdf63712b2a260ff159c1
xutai <xutai@github.com>
2020-09-21 16:00:50 +0800

    first commit

--- mixins/set-header.mjs
+++ mixins/set-header.mjs
@@ -2,19 +2,23 @@
 import { whitelistOrigins } from '../config/whitelistOrigins.mjs'
 
 const setHeaderPreset = (req, res, data = 'preflight not allowed') => {
-    console.info("req.headers.origin", req.headers.origin)
-    if (whitelistOrigins.includes(req.headers.origin)) {
-        res.setHeader('access-control-allow-origin', req.headers.origin)
-        res.setHeader('vary', 'origin')
-        res.setHeader('access-control-allow-credentials', true)
-        // case sensitive
-        res.setHeader('access-control-allow-methods', 'POST, GET, OPTIONS')
-        res.setHeader('access-control-allow-headers', 'framework-i-use, Content-Type')
-        res.setHeader('Access-Control-Max-Age', 86400)
-        console.trace("res.headers", res.headers)
-    } else {
-        res.end(data)
-    }
+    (async () => {
+        console.info("req.headers.origin", req.headers.origin)
+        const isOriginAllowed = await whitelistOrigins.includes(req.headers.origin)
+        if (isOriginAllowed) {
+            res.setHeader('access-control-allow-origin', req.headers.origin)
+            res.setHeader('vary', 'origin')
+            res.setHeader('access-control-allow-credentials', true)
+            // case sensitive
+            res.setHeader('access-control-allow-methods', 'POST, GET, OPTIONS')
+            res.setHeader('access-control-allow-headers', 'framework-i-use, Content-Type')
+            res.setHeader('Access-Control-Max-Age', 86400)
+            console.trace("res.headers", res.headers)
+        } else {
+            res.end(data)
+        }
+    })()
+
 }
 const setCorsHeaderPreset = (req, res, data = 'preflight not allowed') => {
     console.info("req.headers.origin", req.headers.origin)
